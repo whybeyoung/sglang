c2573b3a8ac71be52b0cd4b5fe8703867ea4ee1c
ybyang <ybyang7@iflytek.com>
2025-05-15 09:31:13 +0800

    Fix scheudler with_profile true

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1942,9 +1942,8 @@ class Scheduler(
                 recv_req.num_steps,
                 recv_req.activities,
                 # # NOTE fix
-                # recv_req.with_stack,
+                recv_req.with_stack,
                 # NOTE temp change back
-                False,
                 recv_req.record_shapes,
                 recv_req.profile_id,
             )
