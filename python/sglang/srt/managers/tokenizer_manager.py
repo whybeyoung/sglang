2600f3264ac47da7fad982c1ef5df9e6953a9c6d
ybyang <ybyang7@iflytek.com>
2025-07-09 13:59:43 +0800

    small feat: add server status at health endpoint
    Signed-off-by: ybyang <ybyang7@iflytek.com>

--- python/sglang/srt/managers/tokenizer_manager.py
+++ python/sglang/srt/managers/tokenizer_manager.py
@@ -49,6 +49,7 @@ import zmq
 import zmq.asyncio
 from fastapi import BackgroundTasks
 
+from sglang.srt.utils import ServerStatus
 from sglang.srt.aio_rwlock import RWLock
 from sglang.srt.configs.model_config import ModelConfig
 from sglang.srt.disaggregation.utils import (
@@ -173,6 +174,8 @@ class TokenizerManager:
         server_args: ServerArgs,
         port_args: PortArgs,
     ):
+        # Server Status
+        self.server_status = ServerStatus.Starting
         # Parse args
         self.server_args = server_args
         self.enable_metrics = server_args.enable_metrics
