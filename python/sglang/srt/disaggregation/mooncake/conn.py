7ce792453c2ff8527fd83f2ab99d46c3c6323c7d
Shangming Cai <caishangming@linux.alibaba.com>
2025-07-28 11:36:49 +0800

    update
    Signed-off-by: Shangming Cai <caishangming@linux.alibaba.com>

--- python/sglang/srt/disaggregation/mooncake/conn.py
+++ python/sglang/srt/disaggregation/mooncake/conn.py
@@ -969,6 +969,14 @@ class MooncakeKVSender(BaseKVSender):
             )
         raise KVTransferError(self.bootstrap_room, failure_reason)
 
+    def abort(self):
+        self.kv_mgr.record_failure(
+            self.bootstrap_room,
+            "Aborted by AbortReq.",
+        )
+        # Explicitly set the status to failure since this request has been aborted
+        self.conclude_state = KVPoll.Failed
+
 
 class MooncakeKVReceiver(BaseKVReceiver):
     _ctx = zmq.Context()
@@ -1275,6 +1283,14 @@ class MooncakeKVReceiver(BaseKVReceiver):
                 self.bootstrap_room, "Failed due to an unknown reason from another rank"
             )
         raise KVTransferError(self.bootstrap_room, failure_reason)
+
+    def abort(self):
+        self.kv_mgr.record_failure(
+            self.bootstrap_room,
+            "Aborted by AbortReq.",
+        )
+        # Explicitly set the status to failure since this request has been aborted
+        self.conclude_state = KVPoll.Failed
 
 
 class MooncakeKVBootstrapServer(BaseKVBootstrapServer):
