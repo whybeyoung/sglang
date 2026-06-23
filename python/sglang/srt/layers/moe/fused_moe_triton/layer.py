5cbfe5e78a7205be90658c43bde7761be9e1cce7
Even Zhou <even.y.zhou@outlook.com>
2026-06-23 17:22:17 +0800

    use standalone group for moe ep

--- python/sglang/srt/layers/moe/fused_moe_triton/layer.py
+++ python/sglang/srt/layers/moe/fused_moe_triton/layer.py
@@ -13,6 +13,7 @@ from torch.nn.parameter import UninitializedParameter
 from sglang.srt.batch_overlap.single_batch_overlap import DownGemmOverlapArgs
 from sglang.srt.batch_overlap.two_batch_overlap import MaybeTboDeepEPDispatcher
 from sglang.srt.distributed import (
+    get_moe_ep_group,
     get_moe_expert_parallel_rank,
     get_moe_expert_parallel_world_size,
     get_moe_tensor_parallel_rank,
@@ -103,7 +104,7 @@ def create_moe_dispatcher(moe_runner_config: MoeRunnerConfig) -> BaseDispatcher:
     ):
         return MaybeTboDeepEPDispatcher(
             group=(
-                get_tp_group().device_group
+                get_moe_ep_group().device_group
                 if not a2a_backend.is_mori()
                 else get_tp_group()
             ),
