6a99bb893fb6fbc5f544edcad9959afd18dd2b0f
Chao Shi <chao.shi@alibaba-inc.com>
2026-07-27 16:49:18 +0800

    Fix a minor type error in _sync_and_check_hybrid_prefetch_result

--- python/sglang/srt/mem_cache/unified_radix_cache.py
+++ python/sglang/srt/mem_cache/unified_radix_cache.py
@@ -1363,7 +1363,7 @@ class UnifiedRadixCache(BasePrefixCache):
         # Their hit lengths are stored in completed_tokens.
         pool_transfers = [
             transfer
-            for transfer in operation.pool_transfers
+            for transfer in operation.pool_transfers or []
             if transfer.indices_from_pool != PoolName.KV
         ]
         hit_pages = (
